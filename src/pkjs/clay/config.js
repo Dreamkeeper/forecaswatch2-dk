23e81e08879fd92896f61bf2050bfcea4a5b5998
Dreamkeeper <dkvasnikov@gmail.com>
2026-06-20 17:06:26 +0300

    Update README links for fork releases

--- src/pkjs/clay/config.js
+++ src/pkjs/clay/config.js
@@ -9,7 +9,7 @@ var config = [
     },
     {
         "type": "text",
-        "defaultValue": "Contribute on <a href=\"https://github.com/mattrossman/forecaswatch2\">GitHub!</a>"
+        "defaultValue": "Contribute on <a href=\"https://github.com/Dreamkeeper/YaForecasWatch2\">GitHub!</a>"
     },
     {
         "type": "section",
@@ -340,7 +340,7 @@ var config = [
                 "label": "Share anonymous telemetry",
                 "messageKey": "telemetryEnabled",
                 "defaultValue": true,
-                "description": "<span style=\"color:#9aa0a6;font-size:0.82em;line-height:1.35;\">Share privacy-respecting weather telemetry to improve reliability and understand usage patterns. Learn more about what gets sent in the <a href=\"https://github.com/mattrossman/forecaswatch2#telemetry\">Telemetry section</a>.</span>"
+                "description": "<span style=\"color:#9aa0a6;font-size:0.82em;line-height:1.35;\">Share privacy-respecting weather telemetry to improve reliability and understand usage patterns. Learn more about what gets sent in the <a href=\"https://github.com/Dreamkeeper/YaForecasWatch2#telemetry\">Telemetry section</a>.</span>"
             },
         ]
     },
